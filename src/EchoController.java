f204806c191de8052821c59c433e04dd619ba3e1
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2025-11-24 22:16:35 -0300

    Refactor

--- src/EchoController.java
+++ src/EchoController.java
@@ -1,18 +1,14 @@
-import java.io.IOException;
-
 public class EchoController implements RequestHandler {
     @Override
-    public String handle(Request request)  {
-        if (!request.getMethod().equals(HttpMethod.POST)) {
-            return Response.of("""
-                    { "message": "Request method must be a POST." }""", Response.Type.JSON);
+    public String handle(Request request) {
+        if (valid(request)) {
+            return Response.echo(request, Response.Type.JSON);
         }
 
-        if (!request.hasBody()) {
-            return Response.of("""
-                    { "message": "Missing body in request." }""", Response.Type.JSON);
-        }
+        return Response.of("Invalid Request. Send a POST with a Body.", Response.Type.JSON);
+    }
 
-        return Response.echo(request, Response.Type.JSON);
+    private static boolean valid(Request request) {
+        return request.getMethod().equals(HttpMethod.POST) && request.hasBody();
     }
 }
--- src/Router.java
+++ src/Router.java
@@ -6,21 +6,16 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 public class Router {
     public static void handle(Socket socket) {
         try (var response = socket.getOutputStream()) {
-            var data = Router.route(socket);
+            var request = Request.of(socket);
+            var data = Router.route(request);
+            IO.println(request);
             response.write(data.getBytes(UTF_8));
         } catch (IOException exception) {
             IO.println("[REQUEST:ERROR] " + exception);
         }
     }
 
-    public static String route(Socket socket) throws IOException {
-        var request = Request.of(socket);
-        IO.println(request);
-
-        return switch (request.getPath()) {
-            case "/greeting" -> new GreetingController().handle(request);
-            case "/message" -> new EchoController().handle(request);
-            default -> new NotFoundController().handle(request);
-        };
+    public static String route(Request request) throws IOException {
+        return Routes.getOrNotFound(request);
     }
 }

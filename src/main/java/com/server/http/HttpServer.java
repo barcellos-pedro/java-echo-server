4d44822590a47c576be40a831539b7321dcf0cc3
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2025-11-25 21:22:59 -0300

    Add Virtual Threads to process connections

--- src/main/java/com/server/http/HttpServer.java
+++ src/main/java/com/server/http/HttpServer.java
@@ -16,12 +16,20 @@ public class HttpServer {
     public void listen(Consumer<Socket> handler) throws IOException {
         IO.println("Server running at http://localhost:" + port);
 
-        try (var server = new ServerSocket(port)) {
+        try (var server = new ServerSocket(port); var executor = Executors.newVirtualThreadPerTaskExecutor()) {
             for (; ; ) {
-                try (var socket = server.accept()) {
-                    handler.accept(socket);
-                }
+                var socket = server.accept();
+                executor.submit(() -> process(handler, socket));
             }
+        }
+    }
+
+    private static void process(Consumer<Socket> handler, Socket socket) {
+        try (socket) {
+            IO.println(Thread.currentThread());
+            handler.accept(socket);
+        } catch (IOException exception) {
+            IO.println("[SOCKET:ERROR] " + exception);
         }
     }
 }
